e03c346e0f0e96d49760356735dc5bf8eb3f84a2
it_akumi <it.akumi1011@gmail.com>
2018-08-23 17:50:48 +0900

    Avoid too many attachments error
    Variable attachments is allocated only once to shorten execution time.
    That's why it should have enough length to append all stacks. But nested
    stacks are not appended to the slice and empty items for them cause too
    many attachments error. They should be omitted.

--- src/list-cfn-stacks.go
+++ src/list-cfn-stacks.go
@@ -21,9 +21,11 @@ type Attachment struct {
 
 func buildMessage(description cloudformation.DescribeStacksOutput) (SlackMessage, error) {
 	attachments := make([]Attachment, len(description.Stacks))
+	nestedStackCount := 0
 	for _, stack := range description.Stacks {
 		// Nested stacks are not added to message
 		if stack.ParentId != nil || stack.RootId != nil {
+			nestedStackCount++
 			continue
 		}
 
@@ -35,7 +37,7 @@ func buildMessage(description cloudformation.DescribeStacksOutput) (SlackMessage
 	message := SlackMessage{
 		Text:         "List of stacks",
 		ResponseType: "ephemeral",
-		Attachments:  attachments,
+		Attachments:  attachments[nestedStackCount:], // Omit empty elements
 	}
 
 	return message, nil
